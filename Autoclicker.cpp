8def3adab661d9d36c4deccca300c8ed7dd1d0e5
loloolk <marc.bondoger@gmail.com>
2024-03-01 15:56:11 -0800

    Created Autoclicker for windows

---
+++ Autoclicker.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <Windows.h>
+
+void menu() {
+    std::cout << "Press the right control to enable and the right shift to disable";
+}
+
+void clicker() {
+    bool click = false;
+
+    while (true) {
+        if (GetAsyncKeyState(VK_LCONTROL)) {
+            click = true;
+        }
+        else if (GetAsyncKeyState(VK_RSHIFT)) {
+            click = false;
+        }
+        if (click == true) {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            Sleep(5);
+        }
+    }
+}
+
+int main() {
+    menu();
+    clicker();
+}
